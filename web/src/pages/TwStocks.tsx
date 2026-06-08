a7d66e9d8a4f544c80810f75289dce4a125fb1be
kewos554321 <410413042@gms.ndhu.edu.tw>
2026-06-08 20:56:37 +0800

    chore: remove Recommendations page and tw-stocks recommendations sub-tab

--- web/src/pages/TwStocks.tsx
+++ web/src/pages/TwStocks.tsx
@@ -2,11 +2,9 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { SubTabNav } from '../components/SubTabNav';
 import { Watchlist } from './Watchlist';
 import { AssetSignals } from '../components/AssetSignals';
-import { Recommendations } from './Recommendations';
 
 const TABS = [
   { to: '/tw-stocks', label: '追蹤清單' },
-  { to: '/tw-stocks/recommendations', label: '推薦' },
   { to: '/tw-stocks/signals', label: '訊號歷史' },
 ];
 
@@ -16,7 +14,6 @@ export function TwStocks() {
       <SubTabNav tabs={TABS} />
       <Routes>
         <Route index element={<Watchlist />} />
-        <Route path="recommendations" element={<Recommendations />} />
         <Route path="signals" element={<AssetSignals assetType="tw_stock" />} />
         <Route path="*" element={<Navigate to="/tw-stocks" replace />} />
       </Routes>
